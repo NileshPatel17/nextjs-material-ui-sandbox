d9d18ffd0c818734a0088cba16b6992aacf5115e
NileshPatel17 <nilesh.nvs@hotmail.com>
2022-12-15 22:54:59 +0530

    3 files modified

--- src/pages/device-add.tsx
+++ src/pages/device-add.tsx
@@ -1,36 +1,143 @@
 import { Box, Card, CardContent } from '@material-ui/core';
+import { Field } from 'formik';
+import { useForm, Controller } from 'react-hook-form';
+import { CheckboxWithLabel, TextField } from 'formik-material-ui';
+
 import React, { ReactElement } from 'react';
+import * as Yup from 'yup';
 
 import { Layout } from '../Layout';
-import { RJSFSchema } from '@rjsf/utils';
-import validator from '@rjsf/validator-ajv8';
-import Form from '@rjsf/core';
+import { StepWizard, IStepWizardValues } from '../components/step-wizard';
 
-// const Form = JSONSchemaForm.default;
+const step0Schema = Yup.object().shape({
+  firstName: Yup.string().required('required'),
+  lastName: Yup.string().required('required'),
+});
 
-const schema: RJSFSchema = {
-  title: 'Todo',
-  type: 'object',
-  required: ['title'],
-  properties: {
-    title: { type: 'string', title: 'Title', default: 'A new task' },
-    done: { type: 'boolean', title: 'Done?', default: false },
+const step1Schema = Yup.object({
+  money: Yup.mixed().when('millionaire', {
+    is: true,
+    then: Yup.number()
+      .required()
+      .min(
+        1_000_000,
+        'Because you said you are a millionaire you need to have 1 million'
+      ),
+    otherwise: Yup.number().required(),
+  }),
+});
+
+const step2Schema = Yup.object({
+  description: Yup.string(),
+});
+
+function Step0Content() {
+  return (
+    <>
+      <Box paddingBottom={2}>
+        <Field
+          fullWidth
+          name="firstName"
+          component={TextField}
+          label="First Name"
+        />
+      </Box>
+      <Box paddingBottom={2}>
+        <Field
+          fullWidth
+          name="lastName"
+          component={TextField}
+          label="Last Name"
+        />
+      </Box>
+      <Box paddingBottom={2}>
+        <Field
+          name="millionaire"
+          type="checkbox"
+          component={CheckboxWithLabel}
+          Label={{ label: 'I am a millionaire' }}
+        />
+      </Box>
+    </>
+  );
+}
+function Step1Content() {
+  return (
+    <>
+      <Box paddingBottom={2}>
+        <Field
+          fullWidth
+          name="money"
+          type="number"
+          component={TextField}
+          label="All the money I have"
+        />
+      </Box>
+    </>
+  );
+}
+function Step2Content() {
+  return (
+    <>
+      <Box paddingBottom={2}>
+        <Field
+          fullWidth
+          name="description"
+          component={TextField}
+          label="Description"
+        />
+      </Box>
+    </>
+  );
+}
+
+const stepWizardData: IStepWizardValues = {
+  initialValues: {
+    firstName: '',
+    lastName: '',
+    millionaire: false,
+    money: 0,
+    description: '',
   },
+  steps: [
+    {
+      key: 0,
+      title: 'Personal Data',
+      validationSchema: step0Schema,
+      backButtonLabel: 'baccck',
+      content: <Step0Content />,
+    },
+    {
+      key: 1,
+      title: 'Bank Accounts',
+      validationSchema: step1Schema,
+      backButtonLabel: 'baccck',
+      content: <Step1Content />,
+    },
+    {
+      key: 2,
+      title: 'More Info',
+      validationSchema: step2Schema,
+      backButtonLabel: 'baccck',
+      content: <Step2Content />,
+    },
+  ],
 };
 
 function MultiStepForm() {
+  const onFormSubmit = (formValues: any) => {
+    console.log(
+      '%c formValues',
+      'font-size: 36px; font-weight: bold',
+      formValues
+    );
+  };
   return (
     <Box>
       <Box>Nilesh Patel</Box>
       <Card>
         <CardContent>
-          <Form
-            schema={schema}
-            validator={validator}
-            onChange={log('changed')}
-            onSubmit={log('submitted')}
-            onError={log('errors')}
-          />
+          <StepWizard values={stepWizardData} onFormSubmit={onFormSubmit} />
         </CardContent>
       </Card>
     </Box>
