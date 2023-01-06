9ddc2a303c388923886b83904f60677b2c76b9e7
NileshPatel17 <nilesh.nvs@hotmail.com>
2023-01-06 16:09:32 +0530

    addind sidebar

---
+++ src/components/side-bar/index.ts
@@ -0,0 +1,103 @@
+import React from 'react';
+
+import Divider from '@material-ui/core/Divider';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import { Box, Collapse, List, ListItem } from '@mui/material';
+
+import { items } from './data';
+
+function SidebarItem({ depthStep = 10, depth = 0, expanded, item, ...rest }) {
+  const [collapsed, setCollapsed] = React.useState(true);
+  const { label, items, Icon, onClick: onClickProp } = item;
+
+  function toggleCollapse() {
+    setCollapsed((prevValue) => !prevValue);
+  }
+
+  function onClick(e) {
+    if (Array.isArray(items)) {
+      toggleCollapse();
+    }
+    if (onClickProp) {
+      onClickProp(e, item);
+    }
+  }
+
+  let expandIcon;
+
+  if (Array.isArray(items) && items.length) {
+    expandIcon = !collapsed ? <ExpandLessIcon /> : <ExpandMoreIcon />;
+  }
+
+  return (
+    <>
+      <ListItem
+        className="sidebar-item"
+        onClick={onClick}
+        button
+        dense
+        {...rest}
+      >
+        <div
+          style={{ paddingLeft: depth * depthStep }}
+          className="sidebar-item-content"
+        >
+          {Icon && <Icon className="sidebar-item-icon" fontSize="small" />}
+          <div className="sidebar-item-text">{label}</div>
+        </div>
+        {expandIcon}
+      </ListItem>
+      <Collapse in={!collapsed} timeout="auto" unmountOnExit>
+        {Array.isArray(items) ? (
+          <List disablePadding dense>
+            {items.map((subItem, index) => (
+              <React.Fragment key={`${subItem.name}${index}`}>
+                {subItem === 'divider' ? (
+                  <Divider style={{ margin: '6px 0' }} />
+                ) : (
+                  <SidebarItem
+                    depth={depth + 1}
+                    depthStep={depthStep}
+                    item={subItem}
+                  />
+                )}
+              </React.Fragment>
+            ))}
+          </List>
+        ) : null}
+      </Collapse>
+    </>
+  );
+}
+
+function Sidebar({ items, depthStep, depth, expanded }) {
+  return (
+    <Box
+      sx={{
+        maxWidth: '240px',
+        height: '100vh',
+        border: '1px solid rgba(0, 0, 0, 0.1)',
+      }}
+    >
+      <List disablePadding dense>
+        {items.map((sidebarItem, index) => (
+          <React.Fragment key={`${sidebarItem.name}${index}`}>
+            {sidebarItem === 'divider' ? (
+              <Divider style={{ margin: '6px 0' }} />
+            ) : (
+              <SidebarItem
+                depthStep={depthStep}
+                depth={depth}
+                expanded={expanded}
+                item={sidebarItem}
+              />
+            )}
+          </React.Fragment>
+        ))}
+      </List>
+    </Box>
+  );
+}
+
+export default Sidebar;
